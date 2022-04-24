ae5e055dd93fb4228cac78a4c63c85b17e23a277
janhendrik hausner <janhendrik.hausner@tchibo.de>
2022-04-24 14:16:33 +0200

    added integration test without test container #patch

---
+++ src/integrationtest/kotlin/de/hennihaus/repositories/IntegrationTest.kt
@@ -0,0 +1,11 @@
+package de.hennihaus.repositories
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class IntegrationTest {
+    @Test
+    fun `should add 2 + 2 correctly`() {
+        2 + 2 shouldBe 4
+    }
+}
