9902d22de1e797e70ec8b734b250d98c3234cf1b
janhendrik hausner <janhendrik.hausner@tchibo.de>
2022-06-05 10:23:30 +0200

    naming bugfix #patch

--- src/test/kotlin/de/hennihaus/objectmothers/ConfigurationObjectMother.kt
+++ src/test/kotlin/de/hennihaus/objectmothers/ConfigurationObjectMother.kt
@@ -8,14 +8,14 @@ object ConfigurationObjectMother {
         protocol: String = "http",
         host: String = "0.0.0.0",
         port: Int = 8080,
-        retries: Int = 2,
+        maxRetries: Int = 2,
         authorizationHeader: String = "Basic dGVzdDp0ZXN0",
         originHeader: String = "http://localhost",
     ) = BrokerConfiguration(
         protocol = protocol,
         host = host,
         port = port,
-        maxRetries = retries,
+        maxRetries = maxRetries,
         authorizationHeader = authorizationHeader,
         originHeader = originHeader,
     )
