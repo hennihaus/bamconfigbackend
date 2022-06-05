e75502c48d01d735c9f85fb1f51a17a5104521cc
janhendrik hausner <janhendrik.hausner@tchibo.de>
2022-06-05 12:41:57 +0200

    removed unused method params #patch

--- src/test/kotlin/de/hennihaus/testutils/MockEngineBuilder.kt
+++ src/test/kotlin/de/hennihaus/testutils/MockEngineBuilder.kt
@@ -2,7 +2,6 @@ package de.hennihaus.testutils
 
 import io.ktor.client.engine.mock.MockEngine
 import io.ktor.client.engine.mock.respond
-import io.ktor.client.request.HttpRequestData
 import io.ktor.http.ContentType
 import io.ktor.http.Headers
 import io.ktor.http.HttpHeaders
@@ -15,9 +14,7 @@ object MockEngineBuilder {
         content: String = "",
         status: HttpStatusCode = HttpStatusCode.OK,
         headers: Headers = headersOf(HttpHeaders.ContentType to listOf("${ContentType.Application.Json}")),
-        assertions: (request: HttpRequestData) -> Unit = {},
     ) = MockEngine {
-        assertions(it)
         respond(
             content = content,
             status = status,
