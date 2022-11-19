305bd20b3de17633fd27668c0797bf1492629250
janhendrik hausner <janhendrik.hausner@tchibo.de>
2022-11-19 07:55:39 +0100

    made tests for BankValidationService runnable #patch

--- src/test/kotlin/de/hennihaus/routes/validations/BankValidationServiceTest.kt
+++ src/test/kotlin/de/hennihaus/routes/validations/BankValidationServiceTest.kt
@@ -2,52 +2,32 @@ package de.hennihaus.routes.validations
 
 import de.hennihaus.bamdatamodel.RatingLevel
 import de.hennihaus.bamdatamodel.objectmothers.BankObjectMother.getAsyncBank
-import de.hennihaus.bamdatamodel.objectmothers.BankObjectMother.getDefaultTeams
 import de.hennihaus.bamdatamodel.objectmothers.BankObjectMother.getSchufaBank
 import de.hennihaus.bamdatamodel.objectmothers.BankObjectMother.getSyncBank
 import de.hennihaus.bamdatamodel.objectmothers.CreditConfigurationObjectMother.getCreditConfigurationWithNoEmptyFields
-import de.hennihaus.bamdatamodel.objectmothers.TeamObjectMother.getFirstTeam
-import de.hennihaus.bamdatamodel.objectmothers.TeamObjectMother.getSecondTeam
-import de.hennihaus.objectmothers.UUIDObjectMother
 import de.hennihaus.routes.mappers.toBankDTO
-import de.hennihaus.routes.mappers.toTeamDTO
 import de.hennihaus.routes.validations.BankValidationService.Companion.BANK_MUST_BE_ACTIVE
 import de.hennihaus.routes.validations.BankValidationService.Companion.CREDIT_CONFIGURATION_MIN_AMOUNT_IN_EUROS
 import de.hennihaus.routes.validations.BankValidationService.Companion.CREDIT_CONFIGURATION_MIN_TERM_IN_MONTHS
-import de.hennihaus.services.TeamService
 import io.kotest.matchers.collections.shouldBeEmpty
 import io.kotest.matchers.collections.shouldContainExactly
 import io.mockk.clearAllMocks
-import io.mockk.coEvery
-import io.mockk.coVerify
-import io.mockk.coVerifyAll
-import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import java.util.UUID
 
 class BankValidationServiceTest {
 
-    private val team = mockk<TeamService>()
-
-    private val classUnderTest = BankValidationService(
-        team = team,
-    )
+    private val classUnderTest = BankValidationService()
 
     @BeforeEach
     fun init() = clearAllMocks()
 
     @Nested
     inner class ValidateBody {
-        @BeforeEach
-        fun init() {
-            coEvery { team.getAllTeamIds() } returns getDefaultTeams().map { it.uuid }
-        }
-
         @Test
-        fun `should return an empty list when schufa bank is valid`() = runBlocking {
+        fun `should return an empty list when schufa bank is valid`() = runBlocking<Unit> {
             val body = getSchufaBank().toBankDTO()
 
             val result: List<String> = classUnderTest.validateBody(
@@ -55,9 +35,6 @@ class BankValidationServiceTest {
             )
 
             result.shouldBeEmpty()
-            coVerifyAll {
-                team.getAllTeamIds()
-            }
         }
 
         @Test
@@ -106,92 +83,6 @@ class BankValidationServiceTest {
             )
 
             result shouldContainExactly listOf("thumbnailUrl must have valid url format")
-        }
-
-        @Test
-        fun `should return a list with one error when team elements are not unique`() = runBlocking {
-            val bank = getSchufaBank().toBankDTO()
-            val body = bank.copy(
-                teams = bank.teams + bank.teams[0],
-            )
-
-            val result: List<String> = classUnderTest.validateBody(
-                body = body,
-            )
-
-            result shouldContainExactly listOf("teams all items must be unique")
-        }
-
-        @Test
-        fun `should return a list with one error when one team has an invalid uuid`() = runBlocking {
-            val bank = getSchufaBank().toBankDTO()
-            val body = bank.copy(
-                teams = bank.teams + listOf(
-                    getFirstTeam().toTeamDTO().copy(
-                        uuid = "invalidUUID",
-                    ),
-                ),
-            )
-
-            val result: List<String> = classUnderTest.validateBody(
-                body = body,
-            )
-
-            result shouldContainExactly listOf("teams[3].uuid must have valid uuid format")
-        }
-
-        @Test
-        fun `should return a list with one error when an old team is missing in synchronous bank `() {
-            runBlocking {
-                coEvery { team.getAllTeamIds() } returns listOf(getFirstTeam().uuid, getSecondTeam().uuid)
-                val bank = getSyncBank().toBankDTO()
-                val body = bank.copy(
-                    teams = listOf(
-                        getFirstTeam().toTeamDTO(),
-                    ),
-                )
-
-                val result: List<String> = classUnderTest.validateBody(
-                    body = body,
-                )
-
-                result shouldContainExactly listOf("teams must contain missing uuids: '${getSecondTeam().uuid}'")
-                coVerify(exactly = 1) { team.getAllTeamIds() }
-            }
-        }
-
-        @Test
-        fun `should return a list with one error when one team is not in synchronous bank `() = runBlocking {
-            coEvery { team.getAllTeamIds() } returns listOf(getFirstTeam().uuid)
-            val bank = getSyncBank().toBankDTO()
-            val body = bank.copy(
-                teams = listOf(
-                    getFirstTeam().toTeamDTO(),
-                    getSecondTeam().toTeamDTO(),
-                ),
-            )
-
-            val result: List<String> = classUnderTest.validateBody(
-                body = body,
-            )
-
-            result shouldContainExactly listOf("teams must not contain uuids: '${getSecondTeam().uuid}'")
-        }
-
-        @Test
-        fun `should return a list with one error when one team not exists and bank is async`() = runBlocking {
-            val bank = getAsyncBank().toBankDTO()
-            val body = bank.copy(
-                teams = listOf(
-                    getFirstTeam(uuid = UUID.fromString(UUIDObjectMother.TEST_UUID_1)).toTeamDTO(),
-                ),
-            )
-
-            val result: List<String> = classUnderTest.validateBody(
-                body = body,
-            )
-
-            result shouldContainExactly listOf("teams[0].uuid must exists")
         }
 
         @Test
