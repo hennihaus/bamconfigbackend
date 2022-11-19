c4a6f60515bcb1fda2a555b549e09cffb89ddc85
janhendrik hausner <janhendrik.hausner@tchibo.de>
2022-11-19 13:15:22 +0100

    added tests for TeamValidationService #patch

--- src/main/kotlin/de/hennihaus/routes/validations/TeamValidationService.kt
+++ src/main/kotlin/de/hennihaus/routes/validations/TeamValidationService.kt
@@ -1,9 +1,15 @@
 package de.hennihaus.routes.validations
 
+import de.hennihaus.bamdatamodel.TeamType
+import de.hennihaus.models.generated.rest.QueryDTO
 import de.hennihaus.models.generated.rest.StudentDTO
 import de.hennihaus.models.generated.rest.TeamDTO
+import de.hennihaus.routes.validations.BankValidationService.Companion.BANK_NAME_MAX_LENGTH
+import de.hennihaus.routes.validations.BankValidationService.Companion.BANK_NAME_MIN_LENGTH
 import de.hennihaus.routes.validations.ValidationService.Companion.JMS_QUEUE_MAX_LENGTH
 import de.hennihaus.routes.validations.ValidationService.Companion.JMS_QUEUE_MIN_LENGTH
+import de.hennihaus.routes.validations.ValidationService.Companion.LIMIT_MAXIMUM
+import de.hennihaus.routes.validations.ValidationService.Companion.LIMIT_MINIMUM
 import de.hennihaus.routes.validations.ValidationService.Companion.NAME_MAX_LENGTH
 import de.hennihaus.routes.validations.ValidationService.Companion.NAME_MIN_LENGTH
 import de.hennihaus.services.BankService
@@ -13,9 +19,13 @@ import de.hennihaus.utils.validations.unique
 import de.hennihaus.utils.validations.uuid
 import io.konform.validation.Validation
 import io.konform.validation.ValidationBuilder
+import io.konform.validation.jsonschema.enum
 import io.konform.validation.jsonschema.maxLength
+import io.konform.validation.jsonschema.maximum
 import io.konform.validation.jsonschema.minLength
+import io.konform.validation.jsonschema.minimum
 import io.konform.validation.jsonschema.uniqueItems
+import io.konform.validation.onEach
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
@@ -25,10 +35,11 @@ import org.koin.core.annotation.Single
 class TeamValidationService(
     private val team: TeamService,
     private val bank: BankService,
-) : ValidationService<TeamDTO> {
+) : ValidationService<TeamDTO, QueryDTO> {
 
     override suspend fun bodyValidation(body: TeamDTO): Validation<TeamDTO> = coroutineScope {
         val asyncValidations = listOf(
+            async { typeValidation(body = body) },
             async { usernameValidation(body = body) },
             async { passwordValidation(body = body) },
             async { jmsQueueValidation(body = body) },
@@ -45,6 +56,62 @@ class TeamValidationService(
             }
             validations.forEach {
                 run(validation = it)
+            }
+        }
+    }
+
+    override suspend fun urlValidation(query: QueryDTO): Validation<QueryDTO> = coroutineScope {
+        val asyncValidations = listOf(
+            async { banksValidation(query = query) },
+        )
+        val validations = asyncValidations.awaitAll()
+
+        Validation {
+            QueryDTO::limit {
+                minimum(minimumInclusive = LIMIT_MINIMUM)
+                maximum(maximumInclusive = LIMIT_MAXIMUM)
+            }
+            QueryDTO::type ifPresent {
+                enum<TeamType>()
+            }
+            QueryDTO::password ifPresent {
+                minLength(length = TEAM_PASSWORD_MIN_LENGTH)
+                maxLength(length = TEAM_PASSWORD_MAX_LENGTH)
+            }
+            QueryDTO::minRequests ifPresent {
+                minimum(minimumInclusive = TEAM_MIN_REQUESTS)
+            }
+            QueryDTO::maxRequests ifPresent {
+                minimum(
+                    minimumInclusive = if (query.minRequests is Long && query.minRequests >= TEAM_MIN_REQUESTS) {
+                        query.minRequests
+                    } else {
+                        TEAM_MIN_REQUESTS
+                    },
+                )
+            }
+            validations.forEach {
+                run(validation = it)
+            }
+        }
+    }
+
+    private suspend fun typeValidation(body: TeamDTO): Validation<TeamDTO> = when (body.type) {
+        TeamType.EXAMPLE.name -> {
+            val isTypeUnique = team.isTypeUnique(
+                id = body.uuid,
+                type = body.type,
+            )
+
+            Validation {
+                TeamDTO::type {
+                    unique(isUnique = isTypeUnique)
+                }
+            }
+        }
+        else -> Validation {
+            TeamDTO::type {
+                enum<TeamType>()
             }
         }
     }
@@ -140,6 +207,29 @@ class TeamValidationService(
         }
     }
 
+    private suspend fun banksValidation(query: QueryDTO): Validation<QueryDTO> = coroutineScope {
+        val bankNameExistsRequests = query.banks?.map { name ->
+            async {
+                name to bank.hasName(name = name)
+            }
+        }
+        val bankNamesExists = bankNameExistsRequests?.awaitAll()
+            ?.filter { (_, exists) -> exists }
+            ?.map { it.first }
+            ?: emptyList()
+
+        Validation {
+            QueryDTO::banks ifPresent {
+                uniqueItems(unique = true)
+                onEach {
+                    minLength(length = BANK_NAME_MIN_LENGTH)
+                    maxLength(length = BANK_NAME_MAX_LENGTH)
+                    oneOf(items = bankNamesExists)
+                }
+            }
+        }
+    }
+
     private fun ValidationBuilder<String>.oldJmsQueue(oldJmsQueue: String?) = addConstraint(
         errorMessage = "must be old $oldJmsQueue",
     ) {
@@ -151,5 +241,6 @@ class TeamValidationService(
         const val TEAM_USERNAME_MAX_LENGTH = 50
         const val TEAM_PASSWORD_MIN_LENGTH = 10
         const val TEAM_PASSWORD_MAX_LENGTH = 50
+        const val TEAM_MIN_REQUESTS = 0L
     }
 }
--- src/test/kotlin/de/hennihaus/routes/validations/TeamValidationServiceTest.kt
+++ src/test/kotlin/de/hennihaus/routes/validations/TeamValidationServiceTest.kt
@@ -1,16 +1,26 @@
 package de.hennihaus.routes.validations
 
+import de.hennihaus.bamdatamodel.TeamType
 import de.hennihaus.bamdatamodel.objectmothers.BankObjectMother.ASYNC_BANK_NAME
 import de.hennihaus.bamdatamodel.objectmothers.BankObjectMother.SCHUFA_BANK_NAME
 import de.hennihaus.bamdatamodel.objectmothers.BankObjectMother.SYNC_BANK_NAME
+import de.hennihaus.bamdatamodel.objectmothers.BankObjectMother.getSchufaBank
 import de.hennihaus.bamdatamodel.objectmothers.TeamObjectMother.getFirstTeam
+import de.hennihaus.objectmothers.TeamQueryObjectMother.getTeamQueryWithEmptyFields
+import de.hennihaus.objectmothers.TeamQueryObjectMother.getTeamQueryWithNonEmptyFields
 import de.hennihaus.routes.mappers.toTeamDTO
+import de.hennihaus.routes.mappers.toTeamQueryDTO
+import de.hennihaus.routes.validations.BankValidationService.Companion.BANK_NAME_MAX_LENGTH
+import de.hennihaus.routes.validations.BankValidationService.Companion.BANK_NAME_MIN_LENGTH
+import de.hennihaus.routes.validations.TeamValidationService.Companion.TEAM_MIN_REQUESTS
 import de.hennihaus.routes.validations.TeamValidationService.Companion.TEAM_PASSWORD_MAX_LENGTH
 import de.hennihaus.routes.validations.TeamValidationService.Companion.TEAM_PASSWORD_MIN_LENGTH
 import de.hennihaus.routes.validations.TeamValidationService.Companion.TEAM_USERNAME_MAX_LENGTH
 import de.hennihaus.routes.validations.TeamValidationService.Companion.TEAM_USERNAME_MIN_LENGTH
 import de.hennihaus.routes.validations.ValidationService.Companion.JMS_QUEUE_MAX_LENGTH
 import de.hennihaus.routes.validations.ValidationService.Companion.JMS_QUEUE_MIN_LENGTH
+import de.hennihaus.routes.validations.ValidationService.Companion.LIMIT_MAXIMUM
+import de.hennihaus.routes.validations.ValidationService.Companion.LIMIT_MINIMUM
 import de.hennihaus.routes.validations.ValidationService.Companion.NAME_MAX_LENGTH
 import de.hennihaus.routes.validations.ValidationService.Companion.NAME_MIN_LENGTH
 import de.hennihaus.services.BankService
@@ -47,6 +57,7 @@ class TeamValidationServiceTest {
     inner class ValidateBody {
         @BeforeEach
         fun init() {
+            coEvery { team.isTypeUnique(id = any(), type = any()) } returns true
             coEvery { team.isUsernameUnique(id = any(), username = any()) } returns true
             coEvery { team.isPasswordUnique(id = any(), password = any()) } returns true
             coEvery { team.isJmsQueueUnique(id = any(), jmsQueue = any()) } returns true
@@ -56,7 +67,9 @@ class TeamValidationServiceTest {
 
         @Test
         fun `should return an empty list when team is valid`() = runBlocking {
-            val body = getFirstTeam().toTeamDTO()
+            val body = getFirstTeam().toTeamDTO().copy(
+                type = TeamType.EXAMPLE.name,
+            )
 
             val result: List<String> = classUnderTest.validateBody(
                 body = body,
@@ -64,6 +77,7 @@ class TeamValidationServiceTest {
 
             result.shouldBeEmpty()
             coVerifyAll {
+                team.isTypeUnique(id = body.uuid, type = body.type)
                 team.isUsernameUnique(id = body.uuid, username = body.username)
                 team.isPasswordUnique(id = body.uuid, password = body.password)
                 team.isJmsQueueUnique(id = body.uuid, jmsQueue = body.jmsQueue)
@@ -98,6 +112,34 @@ class TeamValidationServiceTest {
             )
 
             result shouldContainExactly listOf("uuid must have valid uuid format")
+        }
+
+        @Test
+        fun `should return a list with one error when example type is not unique`() = runBlocking {
+            coEvery { team.isTypeUnique(id = any(), type = any()) } returns false
+            val body = getFirstTeam().toTeamDTO().copy(
+                type = TeamType.EXAMPLE.name,
+            )
+
+            val result: List<String> = classUnderTest.validateBody(
+                body = body,
+            )
+
+            result shouldContainExactly listOf("type must be unique")
+            coVerify(exactly = 1) { team.isTypeUnique(id = body.uuid, type = body.type) }
+        }
+
+        @Test
+        fun `should return a list with one error when type has no enum value`() = runBlocking {
+            val body = getFirstTeam().toTeamDTO().copy(
+                type = "invalidType",
+            )
+
+            val result: List<String> = classUnderTest.validateBody(
+                body = body,
+            )
+
+            result shouldContainExactly listOf("type must be one of: 'REGULAR', 'EXAMPLE'")
         }
 
         @Test
@@ -359,6 +401,228 @@ class TeamValidationServiceTest {
             )
 
             result shouldContainExactly listOf("students[0].lastname must have at most $NAME_MAX_LENGTH characters")
+        }
+    }
+
+    @Nested
+    inner class ValidateUrl {
+        @BeforeEach
+        fun init() {
+            coEvery { bank.hasName(name = any()) } returns true
+        }
+
+        @Test
+        fun `should return an empty list when query with max fields set is valid`() = runBlocking<Unit> {
+            val query = getTeamQueryWithNonEmptyFields().toTeamQueryDTO()
+
+            val result: List<String> = classUnderTest.validateUrl(
+                query = query,
+            )
+
+            result.shouldBeEmpty()
+            coVerifyAll {
+                bank.hasName(name = SCHUFA_BANK_NAME)
+                bank.hasName(name = SYNC_BANK_NAME)
+                bank.hasName(name = ASYNC_BANK_NAME)
+            }
+        }
+
+        @Test
+        fun `should return an empty list when query with min fields set is valid`() = runBlocking<Unit> {
+            val query = getTeamQueryWithEmptyFields().toTeamQueryDTO()
+
+            val result: List<String> = classUnderTest.validateUrl(
+                query = query,
+            )
+
+            result.shouldBeEmpty()
+        }
+
+        @Test
+        fun `should return a list with one error when limit is too small`() = runBlocking {
+            val query = getTeamQueryWithNonEmptyFields().toTeamQueryDTO().copy(
+                limit = LIMIT_MINIMUM.dec(),
+            )
+
+            val result: List<String> = classUnderTest.validateUrl(
+                query = query,
+            )
+
+            result shouldContainExactly listOf("limit must be at least '$LIMIT_MINIMUM'")
+        }
+
+        @Test
+        fun `should return a list with one error when limit is too high`() = runBlocking {
+            val query = getTeamQueryWithNonEmptyFields().toTeamQueryDTO().copy(
+                limit = LIMIT_MAXIMUM.inc(),
+            )
+
+            val result: List<String> = classUnderTest.validateUrl(
+                query = query,
+            )
+
+            result shouldContainExactly listOf("limit must be at most '$LIMIT_MAXIMUM'")
+        }
+
+        @Test
+        fun `should return a list with one error when type has no enum value`() = runBlocking {
+            val query = getTeamQueryWithNonEmptyFields().toTeamQueryDTO().copy(
+                type = "invalidType",
+            )
+
+            val result: List<String> = classUnderTest.validateUrl(
+                query = query,
+            )
+
+            result shouldContainExactly listOf("type must be one of: 'REGULAR', 'EXAMPLE'")
+        }
+
+        @Test
+        fun `should return a list with one error when password is too short`() = runBlocking {
+            val query = getTeamQueryWithNonEmptyFields().toTeamQueryDTO().copy(
+                password = Arb.string(size = TEAM_PASSWORD_MIN_LENGTH.dec()).single(),
+            )
+
+            val result: List<String> = classUnderTest.validateUrl(
+                query = query,
+            )
+
+            result shouldContainExactly listOf("password must have at least $TEAM_PASSWORD_MIN_LENGTH characters")
+        }
+
+        @Test
+        fun `should return a list with one error when password is too long`() = runBlocking {
+            val query = getTeamQueryWithNonEmptyFields().toTeamQueryDTO().copy(
+                password = Arb.string(size = TEAM_PASSWORD_MAX_LENGTH.inc()).single(),
+            )
+
+            val result: List<String> = classUnderTest.validateUrl(
+                query = query,
+            )
+
+            result shouldContainExactly listOf("password must have at most $TEAM_PASSWORD_MAX_LENGTH characters")
+        }
+
+        @Test
+        fun `should return a list with one error when minRequests is negative`() = runBlocking {
+            val query = getTeamQueryWithNonEmptyFields().toTeamQueryDTO().copy(
+                minRequests = TEAM_MIN_REQUESTS.dec(),
+            )
+
+            val result: List<String> = classUnderTest.validateUrl(
+                query = query,
+            )
+
+            result shouldContainExactly listOf("minRequests must be at least '$TEAM_MIN_REQUESTS'")
+        }
+
+        @Test
+        fun `should return a list with one error when maxRequests is negative`() = runBlocking {
+            val query = getTeamQueryWithNonEmptyFields().toTeamQueryDTO().copy(
+                maxRequests = TEAM_MIN_REQUESTS.dec(),
+            )
+
+            val result: List<String> = classUnderTest.validateUrl(
+                query = query,
+            )
+
+            result shouldContainExactly listOf("maxRequests must be at least '$TEAM_MIN_REQUESTS'")
+        }
+
+        @Test
+        fun `should return a list with one error when minRequests is null and maxRequests is negative`() = runBlocking {
+            val query = getTeamQueryWithNonEmptyFields().toTeamQueryDTO().copy(
+                minRequests = null,
+                maxRequests = TEAM_MIN_REQUESTS.dec(),
+            )
+
+            val result: List<String> = classUnderTest.validateUrl(
+                query = query,
+            )
+
+            result shouldContainExactly listOf("maxRequests must be at least '$TEAM_MIN_REQUESTS'")
+        }
+
+        @Test
+        fun `should return a list with one error when maxRequests smaller minRequests`() = runBlocking {
+            val query = getTeamQueryWithNonEmptyFields().toTeamQueryDTO().copy(
+                minRequests = 1L,
+                maxRequests = 0L,
+            )
+
+            val result: List<String> = classUnderTest.validateUrl(
+                query = query,
+            )
+
+            result shouldContainExactly listOf("maxRequests must be at least '1'")
+        }
+
+        @Test
+        fun `should return a list with one error when bank elements are not unique`() = runBlocking {
+            val query = getTeamQueryWithNonEmptyFields().toTeamQueryDTO().copy(
+                banks = listOf(
+                    getSchufaBank().name,
+                    getSchufaBank().name,
+                ),
+            )
+
+            val result: List<String> = classUnderTest.validateUrl(
+                query = query,
+            )
+
+            result shouldContainExactly listOf("banks all items must be unique")
+        }
+
+        @Test
+        fun `should return a list with one error when one bank name is too short`() = runBlocking {
+            val query = getTeamQueryWithNonEmptyFields().toTeamQueryDTO().copy(
+                banks = listOf(
+                    element = Arb.string(size = BANK_NAME_MIN_LENGTH.dec()).single(),
+                ),
+            )
+
+            val result: List<String> = classUnderTest.validateUrl(
+                query = query,
+            )
+
+            result shouldContainExactly listOf("banks[0] must have at least $BANK_NAME_MIN_LENGTH characters")
+        }
+
+        @Test
+        fun `should return a list with one error when one bank name is too long`() = runBlocking {
+            val query = getTeamQueryWithNonEmptyFields().toTeamQueryDTO().copy(
+                banks = listOf(
+                    element = Arb.string(size = BANK_NAME_MAX_LENGTH.inc()).single(),
+                ),
+            )
+
+            val result: List<String> = classUnderTest.validateUrl(
+                query = query,
+            )
+
+            result shouldContainExactly listOf("banks[0] must have at most $BANK_NAME_MAX_LENGTH characters")
+        }
+
+        @Test
+        fun `should return a list with one error when one bank does not exist`() = runBlocking {
+            coEvery { bank.hasName(name = any()) } returnsMany listOf(true, true, true, false)
+            val query = getTeamQueryWithNonEmptyFields().toTeamQueryDTO().copy(
+                banks = getTeamQueryWithNonEmptyFields().banks!! + listOf(
+                    "unknownBankName"
+                ),
+            )
+
+            val result: List<String> = classUnderTest.validateUrl(
+                query = query,
+            )
+
+            result shouldContainExactly listOf("banks[3] must exists")
+            coVerifyAll {
+                bank.hasName(name = SCHUFA_BANK_NAME)
+                bank.hasName(name = SYNC_BANK_NAME)
+                bank.hasName(name = ASYNC_BANK_NAME)
+                bank.hasName(name = "unknownBankName")
+            }
         }
     }
 }
