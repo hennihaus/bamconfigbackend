1220564e1a288a8d3b5612dd37e279b13581a962
Jan-Hendrik Hausner <janhendrik.hausner@tchibo.de>
2024-06-20 11:22:25 +0200

    add files for local developement

--- src/main/kotlin/de/hennihaus/plugins/Cors.kt
+++ src/main/kotlin/de/hennihaus/plugins/Cors.kt
@@ -1,7 +1,9 @@
 package de.hennihaus.plugins
 
-import de.hennihaus.configurations.Configuration.ALLOWED_FRONTEND_HOST
-import de.hennihaus.configurations.Configuration.ALLOWED_FRONTEND_PROTOCOL
+import de.hennihaus.configurations.Configuration.ALLOWED_FIRST_FRONTEND_HOST
+import de.hennihaus.configurations.Configuration.ALLOWED_FIRST_FRONTEND_PROTOCOL
+import de.hennihaus.configurations.Configuration.ALLOWED_SECOND_FRONTEND_HOST
+import de.hennihaus.configurations.Configuration.ALLOWED_SECOND_FRONTEND_PROTOCOL
 import de.hennihaus.configurations.Configuration.ALLOWED_SWAGGER_HOST
 import de.hennihaus.configurations.Configuration.ALLOWED_SWAGGER_PROTOCOL
 import io.ktor.http.HttpHeaders
@@ -11,15 +13,18 @@ import io.ktor.server.application.install
 import io.ktor.server.plugins.cors.routing.CORS
 
 fun Application.configureCors() {
-    val allowedFrontendProtocol = environment.config.property(path = ALLOWED_FRONTEND_PROTOCOL).getString()
-    val allowedFrontendHost = environment.config.property(path = ALLOWED_FRONTEND_HOST).getString()
+    val allowedFirstFrontendProtocol = environment.config.property(path = ALLOWED_FIRST_FRONTEND_PROTOCOL).getString()
+    val allowedFirstFrontendHost = environment.config.property(path = ALLOWED_FIRST_FRONTEND_HOST).getString()
+    val allowedSecondFrontendProtocol = environment.config.property(path = ALLOWED_SECOND_FRONTEND_PROTOCOL).getString()
+    val allowedSecondFrontendHost = environment.config.property(path = ALLOWED_SECOND_FRONTEND_HOST).getString()
     val allowedSwaggerProtocol = environment.config.property(path = ALLOWED_SWAGGER_PROTOCOL).getString()
     val allowedSwaggerHost = environment.config.property(path = ALLOWED_SWAGGER_HOST).getString()
 
     install(plugin = CORS) {
         allowCredentials = true
 
-        allowHost(host = allowedFrontendHost, schemes = listOf(allowedFrontendProtocol))
+        allowHost(host = allowedFirstFrontendHost, schemes = listOf(allowedFirstFrontendProtocol))
+        allowHost(host = allowedSecondFrontendHost, schemes = listOf(allowedSecondFrontendProtocol))
         allowHost(host = allowedSwaggerHost, schemes = listOf(allowedSwaggerProtocol))
 
         allowHeader(header = HttpHeaders.ContentType)
